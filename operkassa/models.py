bafb0701dca44eab9ef0eaac8581c76ad059ec9a
alexvydrin <tapat@mail.ru>
2019-01-02 23:20:17 +0300

    first commit

---
+++ operkassa/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+class Oper(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    sum_debet = models.DecimalField(decimal_places=2, max_digits=12)
+    sum_credit = models.DecimalField(decimal_places=2, max_digits=12)
+    text = models.TextField()
+    date = models.DateField()
+    created_date = models.DateTimeField(default=timezone.now)
+    edited_date = models.DateTimeField(default=timezone.now)
+    zakaz = models.CharField(max_length=10)
+    st_credit = models.CharField(max_length=20)
