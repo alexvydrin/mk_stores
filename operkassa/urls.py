db46878bd01396b58b6e58945b25f6dded431583
alexvydrin <tapat@mail.ru>
2019-01-17 22:46:42 +0300

    Added oper_detail

--- operkassa/urls.py
+++ operkassa/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.oper_list, name='oper_list'),
+    re_path(r'^oper/(?P<pk>\d+)/$', views.oper_detail, name='oper_detail'),
 ]
 
--- operkassa/views.py
+++ operkassa/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Oper
+from django.shortcuts import render, get_object_or_404
 
 def oper_list(request):
     #opers = Oper.objects.filter(date__gte=0).order_by('date')
@@ -14,3 +15,7 @@ def oper_list(request):
         'sum_debet_total': sum_debet_total,
         'sum_credit_total': sum_credit_total}
     return render(request, 'operkassa/oper_list.html', context=context)
+
+def oper_detail(request, pk):
+    oper = get_object_or_404(Oper, pk=pk)
+    return render(request, 'operkassa/oper_detail.html', {'oper': oper})
